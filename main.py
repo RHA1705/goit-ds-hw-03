d6c65e8f84f017450d737f8cd48faf103fb378e1
RHA1705 <garbarzroman47@gmail.com>
2025-04-19 06:30:07 +0200

    next

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import connect
 from models import Cat
 
 
+
 def read_all():
-    result = Cat.objects()
-    return result
+    for cat in Cat.objects():
+        print(f"Name: {cat.name}, Age: {cat.age}, Features: {cat.features}")
 
 if __name__ == "__main__":
     read_all()
